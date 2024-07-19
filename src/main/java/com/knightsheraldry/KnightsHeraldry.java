003ba5a079a23c144bc8b801dbe21250be10e206
Banduty <bandutysmods@gmail.com>
2024-07-19 22:18:48 +0200

    Added Datagen, First Example Item and ItemGroups

--- src/main/java/com/knightsheraldry/KnightsHeraldry.java
+++ src/main/java/com/knightsheraldry/KnightsHeraldry.java
@@ -1,20 +1,34 @@
 package com.knightsheraldry;
 
+import com.knightsheraldry.datagen.ModModelProvider;
+import com.knightsheraldry.items.ModItemGroups;
+import com.knightsheraldry.items.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KnightsHeraldry implements ModInitializer, ClientModInitializer {
+public class KnightsHeraldry implements ModInitializer, ClientModInitializer, DataGeneratorEntrypoint {
     public static final String MOD_ID = "knightsheraldry";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        ModItems.registerModItems();
+        ModItemGroups.registerItemGroups();
     }
 
     @Override
     public void onInitializeClient() {
 
+    }
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        pack.addProvider(ModModelProvider::new);
     }
 }
