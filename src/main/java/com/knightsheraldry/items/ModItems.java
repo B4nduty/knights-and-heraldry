a5d084d31c385d8f4fd1e55ca3c640eb3024d316
Banduty <bandutysmods@gmail.com>
2024-07-19 22:39:22 +0200

    Slower weapon_1 attack speed

--- src/main/java/com/knightsheraldry/items/ModItems.java
+++ src/main/java/com/knightsheraldry/items/ModItems.java
@@ -3,13 +3,15 @@ package com.knightsheraldry.items;
 import com.knightsheraldry.KnightsHeraldry;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item WEAPON_1 = registerItem("weapon_1",
-            new Item(new FabricItemSettings()));
+            new SwordItem(ToolMaterials.WOOD, 1, -2.4F, new FabricItemSettings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(KnightsHeraldry.MOD_ID, name), item);
