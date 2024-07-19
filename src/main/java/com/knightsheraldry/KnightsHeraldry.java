82e1c603992344ca74d6a488268aa864a9ce3470
Calum <contact@mineblock11.dev>
2024-07-19 16:32:45 +0100

    Initial Commit

---
+++ src/main/java/com/knightsheraldry/KnightsHeraldry.java
@@ -0,0 +1,20 @@
+package com.knightsheraldry;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KnightsHeraldry implements ModInitializer, ClientModInitializer {
+    public static final String MOD_ID = "knightsheraldry";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
