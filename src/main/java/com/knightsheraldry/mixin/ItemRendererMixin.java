b107259fddfa9db81059a8402ef1c9bf50751b70
Banduty <bandutysmods@gmail.com>
2024-07-31 15:37:19 +0200

    Weapons Part 6

--- src/main/java/com/knightsheraldry/mixin/ItemRendererMixin.java
+++ src/main/java/com/knightsheraldry/mixin/ItemRendererMixin.java
@@ -9,8 +9,12 @@ import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -18,54 +22,55 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public abstract class ItemRendererMixin {
     @ModifyVariable(
             method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
-            at = @At(value = "HEAD"), argsOnly = true
+            at = @At(value = "HEAD"),
+            argsOnly = true
     )
-    public BakedModel use1st3dPersonModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+    public BakedModel use1st3dPersonModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode,
+                                          boolean leftHanded, MatrixStack matrices,
+                                          VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        ClientWorld world = client.world;
+        Entity entity = client.cameraEntity;
+
         if (renderMode == ModelTransformationMode.GUI) {
             return value;
         }
 
-        boolean isBlocking = false;
-        if (MinecraftClient.getInstance().player != null) {
-            isBlocking = MinecraftClient.getInstance().player.isBlocking();
-        }
+        String modelPath = determineModelPath(stack);
 
-        String customModelDataSuffix = stack.getOrCreateNbt().getInt("CustomModelData") == 1 ? "_bludgeoning" : "";
-        String blockingSuffix = isBlocking ? "_blocking" : "";
-        String modelPath = "";
-
-        if (stack.isOf(ModItems.DAGGER)) {
-            modelPath += "dagger_3d";
-        } else if (stack.isOf(ModItems.STILETTO)) {
-            modelPath += "stiletto_3d";
-        } else if (stack.isOf(ModItems.RAPIER)) {
-            modelPath += "rapier_3d";
-        } else if (stack.isOf(ModItems.SWORD)) {
-            modelPath += "sword_3d" + customModelDataSuffix + blockingSuffix;
-        } else if (stack.isOf(ModItems.V_SWORD)) {
-            modelPath += "v_sword_3d" + customModelDataSuffix + blockingSuffix;
-        } else if (stack.isOf(ModItems.ARMING_SWORD)) {
-            modelPath += "arming_sword_3d" + customModelDataSuffix + blockingSuffix;
-        } else if (stack.isOf(ModItems.AXE)) {
-            modelPath += "axe_3d" + blockingSuffix;
-        } else if (stack.isOf(ModItems.BROAD_AXE)) {
-            modelPath += "broad_axe_3d" + blockingSuffix;
-        } else if (stack.isOf(ModItems.CROOKED_AXE)) {
-            modelPath += "crooked_axe_3d" + blockingSuffix;
-        } else if (stack.isOf(ModItems.STRAIGHT_CROOKED_AXE)) {
-            modelPath += "straight_crooked_axe_3d" + blockingSuffix;
-        } else if (stack.isOf(ModItems.WARSWORD)) {
-            modelPath += "warsword_3d" + customModelDataSuffix + blockingSuffix;
-        } else if (stack.isOf(ModItems.WARSWORD_CLAYMORE)) {
-            modelPath += "warsword_claymore_3d" + customModelDataSuffix + blockingSuffix;
-        } else if (stack.isOf(ModItems.WARSWORD_FLAMBERGE)) {
-            modelPath += "warsword_flamberge_3d" + customModelDataSuffix + blockingSuffix;
-        } else if (stack.isOf(ModItems.WARSWORD_ZWEIHANDER)) {
-            modelPath += "warsword_zweihander_3d" + customModelDataSuffix + blockingSuffix;
-        } else {
+        if (modelPath.isEmpty()) {
             return value;
         }
 
-        return ((ItemRendererAccessor) this).knightsAndHereldry$getModels().getModelManager().getModel(new ModelIdentifier(KnightsHeraldry.MOD_ID, modelPath, "inventory"));
+        BakedModel model3d = ((ItemRendererAccessor) this)
+                .knightsAndHereldry$getModels()
+                .getModelManager()
+                .getModel(new ModelIdentifier(KnightsHeraldry.MOD_ID, modelPath, "inventory"));
+
+        if (entity instanceof LivingEntity livingEntity) {
+            BakedModel overriddenModel = model3d.getOverrides().apply(model3d, stack, world, livingEntity, 0);
+            return overriddenModel != null ? overriddenModel : model3d;
+        }
+
+        return value;
+    }
+
+    @Unique
+    private String determineModelPath(ItemStack stack) {
+        if (stack.isOf(ModItems.DAGGER)) return "dagger_3d";
+        if (stack.isOf(ModItems.STILETTO)) return "stiletto_3d";
+        if (stack.isOf(ModItems.RAPIER)) return "rapier_3d";
+        if (stack.isOf(ModItems.SWORD)) return "sword_3d";
+        if (stack.isOf(ModItems.V_SWORD)) return "v_sword_3d";
+        if (stack.isOf(ModItems.ARMING_SWORD)) return "arming_sword_3d";
+        if (stack.isOf(ModItems.AXE)) return "axe_3d";
+        if (stack.isOf(ModItems.BROAD_AXE)) return "broad_axe_3d";
+        if (stack.isOf(ModItems.CROOKED_AXE)) return "crooked_axe_3d";
+        if (stack.isOf(ModItems.STRAIGHT_CROOKED_AXE)) return "straight_crooked_axe_3d";
+        if (stack.isOf(ModItems.WARSWORD)) return "warsword_3d";
+        if (stack.isOf(ModItems.WARSWORD_CLAYMORE)) return "warsword_claymore_3d";
+        if (stack.isOf(ModItems.WARSWORD_FLAMBERGE)) return "warsword_flamberge_3d";
+        if (stack.isOf(ModItems.WARSWORD_ZWEIHANDER)) return "warsword_zweihander_3d";
+        return "";
     }
 }
