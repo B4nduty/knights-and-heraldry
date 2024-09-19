f9ed7eed23dfed34c27b946c1d244f535bf33516
Banduty <bandutysmods@gmail.com>
2024-09-20 01:04:35 +0200

    Alpha 1

--- src/main/java/com/knightsheraldry/mixin/LivingEntityMixin.java
+++ src/main/java/com/knightsheraldry/mixin/LivingEntityMixin.java
@@ -10,9 +10,7 @@ import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,8 +20,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @Shadow @Nullable public abstract DamageSource getRecentDamageSource();
-
     @Inject(method = "jump", at = @At("HEAD"), cancellable = true)
     private void onJump(CallbackInfo ci) {
         LivingEntity entity = (LivingEntity)(Object) this;
