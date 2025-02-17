841d16df1e1464be79b3e62f4be63304e7acd88c
Banduty <bandutysmods@gmail.com>
2025-02-17 01:06:33 +0100

    Version 0.3.3

--- src/main/java/com/knightsheraldry/mixin/ReachEntityAttributesMixin.java
+++ src/main/java/com/knightsheraldry/mixin/ReachEntityAttributesMixin.java
@@ -4,7 +4,6 @@ import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
 import com.knightsheraldry.items.custom.item.KHWeapon;
 import com.knightsheraldry.util.weaponutil.KHWeaponUtil;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ReachEntityAttributesMixin  {
     @Inject(method = "getReachDistance", at = @At("HEAD"), cancellable = true)
     private static void knightsheraldry$getReachDistance(LivingEntity entity, double baseReachDistance, CallbackInfoReturnable<Double> cir) {
-        if (entity.getMainHandStack().getItem() instanceof KHWeapon khWeapon && entity instanceof PlayerEntity player) {
+        if (entity.getMainHandStack().getItem() instanceof KHWeapon khWeapon) {
             var reachDistance = KHWeaponUtil.getMaxDistance(khWeapon);
             cir.setReturnValue(reachDistance);
         }
