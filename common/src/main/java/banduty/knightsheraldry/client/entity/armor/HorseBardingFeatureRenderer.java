b6953fe5b960450711335387c96a7b7aceb92787
phuccom000 <gdtitans6@gmail.com>
2026-04-11 17:18:31 +0700

    trying lazy initialization for the horse barding model

--- common/src/main/java/banduty/knightsheraldry/client/entity/armor/HorseBardingFeatureRenderer.java
+++ common/src/main/java/banduty/knightsheraldry/client/entity/armor/HorseBardingFeatureRenderer.java
@@ -20,26 +20,38 @@ import net.minecraft.world.item.ItemStack;
 
 public class HorseBardingFeatureRenderer extends RenderLayer<Horse, HorseModel<Horse>> {
     private static final ResourceLocation HORSE_ARMOR_TEXTURE_PLUME = new ResourceLocation(KnightsHeraldry.MOD_ID, "textures/entity/horse/armor/horse_barding_plume.png");
-    private final HorseBardingModel<Horse> armorModel;
+    private HorseBardingModel<Horse> armorModel;
+    private final EntityModelSet entityModelSet;
 
     public HorseBardingFeatureRenderer(RenderLayerParent<Horse, HorseModel<Horse>> renderLayerParent, EntityModelSet entityModelSet) {
         super(renderLayerParent);
-        this.armorModel = new HorseBardingModel<>(entityModelSet.bakeLayer(ModEntityModelLayers.HORSE_BARDING_MODEL_LAYER));
+        this.entityModelSet = entityModelSet;
+        // Don't bake here - wait until first use
+    }
+
+    private HorseBardingModel<Horse> getArmorModel() {
+        if (this.armorModel == null) {
+            // Bake the model when first needed (after layers are registered)
+            this.armorModel = new HorseBardingModel<>(entityModelSet.bakeLayer(ModEntityModelLayers.HORSE_BARDING_MODEL_LAYER));
+        }
+        return this.armorModel;
     }
 
     @Override
     public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int light, Horse horse, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
         ItemStack armorStack = horse.getArmor();
         if (!armorStack.isEmpty() && armorStack.getItem() instanceof HorseBardingArmorItem horseBardingArmorItem) {
-            this.getParentModel().copyPropertiesTo(this.armorModel);
+            HorseBardingModel<Horse> model = getArmorModel(); // Get model lazily
 
-            this.armorModel.prepareMobModel(horse, limbAngle, limbDistance, tickDelta);
-            this.armorModel.setupAnim(horse, limbAngle, limbDistance, animationProgress, headYaw, headPitch);
+            this.getParentModel().copyPropertiesTo(model);
+
+            model.prepareMobModel(horse, limbAngle, limbDistance, tickDelta);
+            model.setupAnim(horse, limbAngle, limbDistance, animationProgress, headYaw, headPitch);
 
             // Base armor texture
             ResourceLocation texture = new ResourceLocation(KnightsHeraldry.MOD_ID, "textures/entity/horse/armor/" + BuiltInRegistries.ITEM.getKey(armorStack.getItem()).getPath() + ".png");
             VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.armorCutoutNoCull(texture));
-            this.armorModel.renderToBuffer(poseStack, vertexConsumer, light, LivingEntityRenderer.getOverlayCoords(horse, 0.0F), 1.0F, 1.0F, 1.0F, 1.0F);
+            model.renderToBuffer(poseStack, vertexConsumer, light, LivingEntityRenderer.getOverlayCoords(horse, 0.0F), 1.0F, 1.0F, 1.0F, 1.0F);
 
             // Overlay texture (dyed color)
             int colorInt = horseBardingArmorItem.getColor(armorStack);
@@ -50,7 +62,7 @@ public class HorseBardingFeatureRenderer extends RenderLayer<Horse, HorseModel<H
             };
             ResourceLocation textureOverlay = new ResourceLocation(KnightsHeraldry.MOD_ID, "textures/entity/horse/armor/" + BuiltInRegistries.ITEM.getKey(armorStack.getItem()).getPath() + "_overlay.png");
             VertexConsumer vertexConsumerOverlay = multiBufferSource.getBuffer(RenderType.armorCutoutNoCull(textureOverlay));
-            this.armorModel.renderToBuffer(poseStack, vertexConsumerOverlay, light, LivingEntityRenderer.getOverlayCoords(horse, 0.0F), colorFloat[0], colorFloat[1], colorFloat[2], 1.0F);
+            model.renderToBuffer(poseStack, vertexConsumerOverlay, light, LivingEntityRenderer.getOverlayCoords(horse, 0.0F), colorFloat[0], colorFloat[1], colorFloat[2], 1.0F);
 
             // Plume texture (optional, dyed color)
             if (armorStack.getTag() != null && armorStack.getTag().contains("plume")) {
@@ -62,7 +74,7 @@ public class HorseBardingFeatureRenderer extends RenderLayer<Horse, HorseModel<H
                     (colorInt & 255) / 255.0F
             };
             VertexConsumer vertexConsumerPlume = multiBufferSource.getBuffer(RenderType.armorCutoutNoCull(HORSE_ARMOR_TEXTURE_PLUME));
-            this.armorModel.plume.render(poseStack, vertexConsumerPlume, light, LivingEntityRenderer.getOverlayCoords(horse, 0.0F), colorFloat[0], colorFloat[1], colorFloat[2], 1.0F);
+            model.plume.render(poseStack, vertexConsumerPlume, light, LivingEntityRenderer.getOverlayCoords(horse, 0.0F), colorFloat[0], colorFloat[1], colorFloat[2], 1.0F);
         }
     }
 }
