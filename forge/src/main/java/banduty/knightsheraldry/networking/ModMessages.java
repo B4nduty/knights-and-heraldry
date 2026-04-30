7c15be2fa17c417b08f0e2bdcd679abc0089a4f0
Banduty <bandutysmods@gmail.com>
2026-05-01 01:42:34 +0200

    Release 1 rev 3

--- forge/src/main/java/banduty/knightsheraldry/networking/ModMessages.java
+++ forge/src/main/java/banduty/knightsheraldry/networking/ModMessages.java
@@ -1,7 +1,7 @@
 package banduty.knightsheraldry.networking;
 
+import banduty.knightsheraldry.KnightsHeraldry;
 import banduty.knightsheraldry.networking.packet.VelocityS2CPacket;
-import banduty.stoneycore.StoneyCore;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
@@ -10,7 +10,7 @@ public class ModMessages {
 
     private static final String PROTOCOL_VERSION = "1";
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
-            new ResourceLocation(StoneyCore.MOD_ID, "main"),
+            new ResourceLocation(KnightsHeraldry.MOD_ID, "main"),
             () -> PROTOCOL_VERSION,
             PROTOCOL_VERSION::equals,
             PROTOCOL_VERSION::equals
