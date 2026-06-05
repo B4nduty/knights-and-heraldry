09b1f6f16de9c699f611effda8826491f80db642
phuccom000 <gdtitans6@gmail.com>
2026-06-05 10:59:12 +0700

    Fixed NeoForge's model bug

--- neoforge/src/main/java/banduty/knightsheraldry/KnightsHeraldryNeoForgeClient.java
+++ neoforge/src/main/java/banduty/knightsheraldry/KnightsHeraldryNeoForgeClient.java
@@ -14,7 +14,6 @@ import banduty.knightsheraldry.model.HorseBardingModel;
 import banduty.knightsheraldry.model.ModEntityModelLayers;
 import banduty.knightsheraldry.util.itemdata.ModModelPredicates;
 import banduty.stoneycore.items.custom.armor.underarmor.SCDyeableUnderArmor;
-import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -84,12 +83,16 @@ public class KnightsHeraldryNeoForgeClient {
                     int defaultColor = -1;
                     switch (stack.getItem()) {
                         case SCDyeableUnderArmor dyeable -> defaultColor = dyeable.getDefaultColor();
-                        case KHChestplateAttachment khChestplateAttachment -> defaultColor = khChestplateAttachment.getDefaultColor();
+                        case KHChestplateAttachment khChestplateAttachment ->
+                                defaultColor = khChestplateAttachment.getDefaultColor();
                         case KHCloak khCloak -> defaultColor = khCloak.getDefaultColor();
-                        case KHLeggingsAttachment khLeggingsAttachment -> defaultColor = khLeggingsAttachment.getDefaultColor();
+                        case KHLeggingsAttachment khLeggingsAttachment ->
+                                defaultColor = khLeggingsAttachment.getDefaultColor();
                         case KHChaperon khChaperon -> defaultColor = khChaperon.getDefaultColor();
-                        case HorseBardingArmorItem horseBardingArmorItem -> defaultColor = horseBardingArmorItem.getDefaultColor();
-                        default -> {}
+                        case HorseBardingArmorItem horseBardingArmorItem ->
+                                defaultColor = horseBardingArmorItem.getDefaultColor();
+                        default -> {
+                        }
                     }
 
                     return DyedItemColor.getOrDefault(stack, defaultColor);
@@ -109,240 +112,5 @@ public class KnightsHeraldryNeoForgeClient {
     @SubscribeEvent
     public static void registerLoaders(ModelEvent.RegisterGeometryLoaders event) {
         event.register(ResourceLocation.fromNamespaceAndPath(KnightsHeraldry.MOD_ID, "surcoat_with_banner"), SurcoatWithBannerModel.INSTANCE);
-    }
-
-    @SubscribeEvent
-    public static void registerAdditionalModels(ModelEvent.RegisterAdditional event) {
-
-        String[] patterns = new String[]{
-                "bl", "bo", "br", "bri", "bs", "bt", "bts", "cbo", "cr", "cre", "cs",
-                "dls", "drs", "flo", "glb", "gra", "gru", "hh", "hhb", "ld", "ls",
-                "lud", "mc", "moj", "mr", "ms", "pig", "rd", "rs", "rud", "sc", "sku",
-                "ss", "tl", "tr", "ts", "tt", "tts", "vh", "vhr"
-        };
-
-        // Register for each base item that supports patterns
-        Item[] patternedItems = new Item[]{
-                KHItems.SURCOAT.get(),
-                KHItems.SURCOAT_SLEEVELESS.get()
-        };
-
-        for (Item item : patternedItems) {
-            ResourceLocation id = BuiltInRegistries.ITEM.getKey(item);
-            for (String pattern : patterns) {
-                ResourceLocation modelLoc = ResourceLocation.fromNamespaceAndPath(id.getNamespace(), "item/" + id.getPath() + "/" + pattern);
-                event.register(ModelResourceLocation.standalone(modelLoc));
-            }
-        }
-
-        // 3D Models
-        Item[] weapons3D = {
-                KHItems.DAGGER.get(),
-                KHItems.STILETTO.get(),
-                KHItems.RAPIER.get(),
-                KHItems.SWORD.get(),
-                KHItems.V_SWORD.get(),
-                KHItems.ARMING_SWORD.get(),
-                KHItems.AXE.get(),
-                KHItems.BROAD_AXE.get(),
-                KHItems.CROOKED_AXE.get(),
-                KHItems.STRAIGHT_CROOKED_AXE.get(),
-                KHItems.MACE.get(),
-                KHItems.SPIKED_MACE.get(),
-                KHItems.HAMMER.get(),
-                KHItems.WAR_HAMMER.get(),
-                KHItems.LONGSWORD.get(),
-                KHItems.V_LONGSWORD.get(),
-                KHItems.FALCHION.get(),
-                KHItems.SCIMITAR.get(),
-                KHItems.PITCHFORK.get(),
-                KHItems.SPEAR.get(),
-                KHItems.PIKE.get(),
-                KHItems.BILLHOOK.get(),
-                KHItems.GLAIVE.get(),
-                KHItems.CURVED_GLAIVE.get(),
-                KHItems.HALBERD.get(),
-                KHItems.LANCE.get(),
-                KHItems.WOODEN_LANCE.get(),
-                KHItems.POLEAXE.get(),
-                KHItems.POLEHAMMER.get(),
-                KHItems.BEC_DE_CORBIN.get(),
-                KHItems.MORNING_STAR.get(),
-                KHItems.BARDICHE.get(),
-                KHItems.GREATSWORD.get(),
-                KHItems.CLAYMORE.get(),
-                KHItems.FLAMBERGE.get(),
-                KHItems.ZWEIHANDER.get(),
-                KHItems.WARDART.get(),
-                KHItems.LONGBOW.get()
-        };
-
-        // 2D ICON-ONLY weapons
-        Item[] weaponsIcon = {
-                KHItems.FLAIL.get(),
-                KHItems.BALL_FLAIL.get(),
-                KHItems.HEAVY_CROSSBOW.get(),
-                KHItems.ARQUEBUS.get(),
-                KHItems.HANDGONNE.get()
-        };
-
-        // ===== register 3D models =====
-        for (Item item : weapons3D) {
-            ResourceLocation id = BuiltInRegistries.ITEM.getKey(item);
-            ResourceLocation modelLoc = ResourceLocation.fromNamespaceAndPath(id.getNamespace(), "item/" + id.getPath() + "_3d");
-            event.register(ModelResourceLocation.standalone(modelLoc));
-        }
-
-        // ===== register icon models =====
-        for (Item item : weaponsIcon) {
-            ResourceLocation id = BuiltInRegistries.ITEM.getKey(item);
-            ResourceLocation modelLoc = ResourceLocation.fromNamespaceAndPath(id.getNamespace(), "item/" + id.getPath() + "_icon");
-            event.register(ModelResourceLocation.standalone(modelLoc));
-        }
-
-        // Manuscript Items
-        Item[] manuscriptItems = {
-                KHItems.DAGGER.get(),
-                KHItems.STILETTO.get(),
-                KHItems.RAPIER.get(),
-                KHItems.SWORD.get(),
-                KHItems.V_SWORD.get(),
-                KHItems.ARMING_SWORD.get(),
-                KHItems.AXE.get(),
-                KHItems.BROAD_AXE.get(),
-                KHItems.CROOKED_AXE.get(),
-                KHItems.STRAIGHT_CROOKED_AXE.get(),
-                KHItems.MACE.get(),
-                KHItems.SPIKED_MACE.get(),
-                KHItems.FLAIL.get(),
-                KHItems.BALL_FLAIL.get(),
-                KHItems.HAMMER.get(),
-                KHItems.WAR_HAMMER.get(),
-                KHItems.LONGSWORD.get(),
-                KHItems.V_LONGSWORD.get(),
-                KHItems.FALCHION.get(),
-                KHItems.SCIMITAR.get(),
-                KHItems.PITCHFORK.get(),
-                KHItems.SPEAR.get(),
-                KHItems.PIKE.get(),
-                KHItems.BILLHOOK.get(),
-                KHItems.GLAIVE.get(),
-                KHItems.CURVED_GLAIVE.get(),
-                KHItems.HALBERD.get(),
-                KHItems.LANCE.get(),
-                KHItems.WOODEN_LANCE.get(),
-                KHItems.POLEAXE.get(),
-                KHItems.POLEHAMMER.get(),
-                KHItems.BEC_DE_CORBIN.get(),
-                KHItems.MORNING_STAR.get(),
-                KHItems.BARDICHE.get(),
-                KHItems.GREATSWORD.get(),
-                KHItems.CLAYMORE.get(),
-                KHItems.FLAMBERGE.get(),
-                KHItems.ZWEIHANDER.get(),
-                KHItems.WARDART.get(),
-
-                KHItems.QUILTED_COIF.get(),
-                KHItems.GAMBESON.get(),
-                KHItems.GAMBESON_BREECHES.get(),
-                KHItems.GAMBESON_BOOTS.get(),
-
-                KHItems.MAIL_COIF.get(),
-                KHItems.HAUBERK.get(),
-                KHItems.MAIL_BREECHES.get(),
-                KHItems.MAIL_BOOTS.get(),
-
-                KHItems.MAIL_SPAULDERS.get(),
-                KHItems.BRIGANDINE_SPAULDERS.get(),
-                KHItems.PLATE_SPAULDERS.get(),
-
-                KHItems.BRIGANDINE.get(),
-
-                KHItems.BRIG_BREASTPLATE.get(),
-                KHItems.BRIG_BREASTPLATE_TASSETS.get(),
-
-                KHItems.PLATE_CUIRASS.get(),
-                KHItems.PLATE_CUIRASS_TASSETS.get(),
-                KHItems.MAXIMILLIAN_CUIRASS.get(),
-                KHItems.MAXIMILLIAN_CUIRASS_TASSETS.get(),
-                KHItems.XIIII_PLATE_CUIRASS.get(),
-                KHItems.XIIII_PLATE_CUIRASS_TASSETS.get(),
-                KHItems.XIIII_PLATE_BREASTPLATE.get(),
-
-                KHItems.BARBUTE.get(),
-                KHItems.BASCINET.get(),
-                KHItems.KETTLE_HELM.get(),
-                KHItems.NASAL_HELM.get(),
-                KHItems.VIKING_HELM.get(),
-                KHItems.BURGONET.get(),
-
-                KHItems.ARMET.get(),
-                KHItems.ARMET_2.get(),
-                KHItems.VISORED_BARBUTE.get(),
-                KHItems.HOUNDSKULL.get(),
-                KHItems.CAGE.get(),
-                KHItems.VISORED_BASCINET.get(),
-                KHItems.GREAT_HELM.get(),
-                KHItems.SALLET.get(),
-                KHItems.BURGONET_FALLING_BUFFE.get(),
-                KHItems.CLOSE_HELM.get(),
-
-                KHItems.FROGMOUTH.get(),
-                KHItems.GREAT_ARMET.get(),
-                KHItems.GREAT_ARMET_2.get(),
-                KHItems.GREAT_BASCINET.get(),
-                KHItems.GREAT_HOUNDSKUL_BASCINET.get(),
-                KHItems.MAXIMILLIAN_HELMET.get(),
-                KHItems.SAVOYARD.get(),
-
-                KHItems.GAUNTLET.get(),
-                KHItems.BRIGANDINE_HARNESS.get(),
-                KHItems.PLATE_HARNESS.get(),
-
-                KHItems.BRIGANDINE_CUISSES.get(),
-                KHItems.PLATE_CUISSES.get(),
-
-                KHItems.GREAVES.get(),
-
-                KHItems.SABATONS.get(),
-
-                KHItems.SURCOAT.get(),
-                KHItems.SURCOAT_SLEEVELESS.get(),
-                KHItems.CIVILIAN_SURCOAT.get(),
-                KHItems.GIORNEA.get(),
-
-                KHItems.CLOAK.get(),
-                KHItems.TORN_CLOAK.get(),
-
-                KHItems.HOOD.get(),
-                KHItems.TORN_HOOD.get(),
-                KHItems.JESTER_HOOD.get(),
-                KHItems.HELMET_HOOD.get(),
-                KHItems.HELMET_TORN_HOOD.get(),
-
-                KHItems.LONGBOW.get(),
-
-                KHItems.HEAVY_CROSSBOW.get(),
-
-                KHItems.ARQUEBUS.get(),
-
-                KHItems.HANDGONNE.get(),
-
-                KHItems.SWALLOWTAIL_ARROW.get(),
-                KHItems.BODKIN_ARROW.get(),
-                KHItems.BROADHEAD_ARROW.get(),
-                KHItems.CLOTH_ARROW.get(),
-
-                KHItems.HORSE_BARDING.get(),
-
-                KHItems.RIM_GUARDS.get(),
-                KHItems.BESAGEWS.get()
-        };
-
-        for (Item item : manuscriptItems) {
-            String itemName = BuiltInRegistries.ITEM.getKey(item).getPath();
-            ResourceLocation modelLoc = ResourceLocation.fromNamespaceAndPath(KnightsHeraldry.MOD_ID, "item/" +"manuscript_" + itemName);
-            event.register(ModelResourceLocation.standalone(modelLoc));
-        }
     }
 }
