8bd3dd442e0af59ea146752ec9272e9254a4a60d
phuccom000 <gdtitans6@gmail.com>
2026-06-14 09:26:18 +0700

    Optimized use() of the attachment classes

--- common/src/main/java/banduty/knightsheraldry/items/armor/attachment/KHBootsAttachment.java
+++ common/src/main/java/banduty/knightsheraldry/items/armor/attachment/KHBootsAttachment.java
@@ -4,11 +4,6 @@ import banduty.knightsheraldry.client.item.armor.KHBootsAttachmentRenderer;
 import banduty.stoneycore.client.render.ArmorAttachmentRenderProvider;
 import banduty.stoneycore.client.render.ArmorAttachmentRenderer;
 import banduty.stoneycore.items.custom.armor.ArmorAttachment;
-import banduty.stoneycore.items.custom.armor.underarmor.SCUnderArmor;
-import banduty.stoneycore.items.custom.armor.underarmor.UnderArmorContents;
-import banduty.stoneycore.util.data.itemdata.SCDataComponents;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -17,8 +12,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
-
-import static banduty.stoneycore.util.SCInventoryItemFinder.findUnderArmor;
 
 public class KHBootsAttachment extends Item implements ArmorAttachment, ArmorAttachmentRenderProvider {
     private final Ingredient ingredient;
@@ -45,50 +38,6 @@ public class KHBootsAttachment extends Item implements ArmorAttachment, ArmorAtt
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        ItemStack stack = player.getItemInHand(hand);
-        if (level.isClientSide) {
-            ItemStack target = findUnderArmor(player, ArmorItem.Type.BOOTS);
-            if (!target.isEmpty()) {
-                return InteractionResultHolder.sidedSuccess(stack, true);
-            }
-            return InteractionResultHolder.pass(stack);
-        }
-
-        ItemStack target = findUnderArmor(player, ArmorItem.Type.BOOTS);
-
-        if (!target.isEmpty() && target.getItem() instanceof SCUnderArmor underArmor) {
-
-            UnderArmorContents contents = target.getOrDefault(SCDataComponents.UNDER_ARMOR_CONTENTS.get(),
-                    UnderArmorContents.EMPTY);
-
-            UnderArmorContents.Mutable mutable = new UnderArmorContents.Mutable(contents);
-
-            ItemStack result = mutable.tryInsert(stack, player, target);
-
-            if (!result.isEmpty()) {
-                player.getInventory().placeItemBackInInventory(result);
-            }
-
-            target.set(
-                    SCDataComponents.UNDER_ARMOR_CONTENTS.get(),
-                    mutable.toImmutable());
-
-            underArmor.rebuildAttachmentAttributes(target);
-
-            level.playSound(
-                    null,
-                    player.blockPosition(),
-                    SoundEvents.ARMOR_EQUIP_LEATHER.value(),
-                    SoundSource.PLAYERS,
-                    1.0F,
-                    1.0F);
-
-            stack.shrink(1);
-
-            return InteractionResultHolder.success(stack);
-
-        }
-
-        return InteractionResultHolder.pass(stack);
+        return ArmorAttachment.super.use(level, player, hand, ArmorItem.Type.BOOTS);
     }
 }
--- common/src/main/java/banduty/knightsheraldry/items/armor/attachment/KHCloak.java
+++ common/src/main/java/banduty/knightsheraldry/items/armor/attachment/KHCloak.java
@@ -6,10 +6,6 @@ import banduty.stoneycore.client.render.ArmorAttachmentRenderProvider;
 import banduty.stoneycore.client.render.ArmorAttachmentRenderer;
 import banduty.stoneycore.items.custom.armor.ArmorAttachment;
 import banduty.stoneycore.items.custom.armor.underarmor.SCUnderArmor;
-import banduty.stoneycore.items.custom.armor.underarmor.UnderArmorContents;
-import banduty.stoneycore.util.data.itemdata.SCDataComponents;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -18,8 +14,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
-
-import static banduty.stoneycore.util.SCInventoryItemFinder.findUnderArmor;
 
 public class KHCloak extends Item implements ArmorAttachment, ArmorAttachmentRenderProvider {
     boolean hasOverlay;
@@ -70,50 +64,6 @@ public class KHCloak extends Item implements ArmorAttachment, ArmorAttachmentRen
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        ItemStack stack = player.getItemInHand(hand);
-        if (level.isClientSide) {
-            ItemStack target = findUnderArmor(player, armorType);
-            if (!target.isEmpty()) {
-                return InteractionResultHolder.sidedSuccess(stack, true);
-            }
-            return InteractionResultHolder.pass(stack);
-        }
-
-        ItemStack target = findUnderArmor(player, armorType);
-
-        if (!target.isEmpty() && target.getItem() instanceof SCUnderArmor underArmor) {
-
-            UnderArmorContents contents = target.getOrDefault(SCDataComponents.UNDER_ARMOR_CONTENTS.get(),
-                    UnderArmorContents.EMPTY);
-
-            UnderArmorContents.Mutable mutable = new UnderArmorContents.Mutable(contents);
-
-            ItemStack result = mutable.tryInsert(stack, player, target);
-
-            if (!result.isEmpty()) {
-                player.getInventory().placeItemBackInInventory(result);
-            }
-
-            target.set(
-                    SCDataComponents.UNDER_ARMOR_CONTENTS.get(),
-                    mutable.toImmutable());
-
-            underArmor.rebuildAttachmentAttributes(target);
-
-            level.playSound(
-                    null,
-                    player.blockPosition(),
-                    SoundEvents.ARMOR_EQUIP_LEATHER.value(),
-                    SoundSource.PLAYERS,
-                    1.0F,
-                    1.0F);
-
-            stack.shrink(1);
-
-            return InteractionResultHolder.success(stack);
-
-        }
-
-        return InteractionResultHolder.pass(stack);
+        return ArmorAttachment.super.use(level, player, hand, armorType);
     }
 }
